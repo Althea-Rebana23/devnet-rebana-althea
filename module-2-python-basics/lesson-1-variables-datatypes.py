d5e46ffda154ef773cbf799a27dafbf4be740ba3
Maria Althea Rebana <m.rebana61171@mcc.edu.ph>
2026-09-26 01:25:30 +0800

    Update lesson-1-variables-datatypes.py

--- module-2-python-basics/lesson-1-variables-datatypes.py
+++ module-2-python-basics/lesson-1-variables-datatypes.py
@@ -1,24 +1,26 @@
 """
 Module 2 — Lesson 1: Variables & Data Types
-Student: [your name]
-Date: [date]
+Student: Rebana, Maria Althea D.
+Date: 9/26/2026
 
 ============================================
 WHAT IS THIS TOPIC? (explain it like you're
 teaching a friend who's never coded before)
 ============================================
-[write your own explanation here]
-
+I think of variables as shoe boxes. The box is the variable while the shoes inside represent the data. 
+Data types tell us what kind of data is being stored in the box.
+Just like a shoe box can contain Nayki or JordanBonia shoes, a variable can contain different types of data such as text, numbers, or true/false values. 
+This helps Python know how to handle the information stored in the variable.
 
 ============================================
 KEY VOCABULARY
 ============================================
-- variable:
-- data type:
-- int:
-- float:
-- string:
-- boolean:
+- variable: used to store data
+- data type: data stored in a variable
+- int: A whole number without a decimal point
+- float: A number with a decimal point
+- string: text data enclosed in quotation marks
+- boolean: data type that has only two value which are True or False.
 (add more as needed)
 
 
@@ -29,15 +31,22 @@ Write at least one working example below that you
 came up with yourself — not copied from class.
 """
 
-# --- your code example goes here ---
-
+# --- My information ---
+name = "Althea"
+age = 21
+my_height = 1.70
+is_minor = False
+print("Name:", name)
+print("Age:", age)
+print("Height:", my_height)
+print("Minor:", is_minor)
 
 """
 ============================================
 A MISTAKE I MADE (or one I want to avoid)
 ============================================
-[what's something confusing or easy to get wrong
-about this topic?]
+I did not find this topic very difficult, but I needed to understand the difference between a variable and a data type.
+I learned that variables store information, while data types describe what kind of information is being stored.
 
 
 ============================================
